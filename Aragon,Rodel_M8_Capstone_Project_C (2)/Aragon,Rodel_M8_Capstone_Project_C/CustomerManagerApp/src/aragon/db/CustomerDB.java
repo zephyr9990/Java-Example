d0345eedab4fa76fb548c82f7e12acba0e333ae3
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2018-08-13 12:06:03 -0700

    Added documentation for updateCustomer function

--- Aragon,Rodel_M8_Capstone_Project_C (2)/Aragon,Rodel_M8_Capstone_Project_C/CustomerManagerApp/src/aragon/db/CustomerDB.java
+++ Aragon,Rodel_M8_Capstone_Project_C (2)/Aragon,Rodel_M8_Capstone_Project_C/CustomerManagerApp/src/aragon/db/CustomerDB.java
@@ -94,6 +94,11 @@ public class CustomerDB {
         return customerDeleted;
     }
     
+    /**
+    * Updates a customer's information from the database.
+    * @param customer The customer to update.
+    * @return True if customer was updated, false if not.
+    */
     public static boolean updateCustomer(Customer customer) throws DBException
     {
         String sql = "UPDATE Customer "
